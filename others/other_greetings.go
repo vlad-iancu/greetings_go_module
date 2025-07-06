ab2beb6bfd6a1dc61de351329dddfef8fc8db2c9
Vlad <iancuvladalexandru@gmail.com>
2025-07-06 22:29:43 +0300

    Subpackage

---
+++ others/other_greetings.go
@@ -0,0 +1,18 @@
+package other_greetings
+
+import (
+	"fmt"
+)
+
+func OtherGreeting(name string) (string, error) {
+	// If the name is empty, return an error.
+	if name == "" {
+		return "", fmt.Errorf("empty name")
+	}
+
+	// Create a message using the name.
+	message := fmt.Sprintf("Hello, %v. Welcome!", name)
+
+	// Return the message and a nil error.
+	return message, nil
+}
